3c7c28260e95e3cd093b90df3de9619b7ed19797
hamza bouthour <61101578+hamza-bouthour@users.noreply.github.com>
2025-08-05 21:05:35 -0700

    Update guestData.ts

--- client/src/data/guestData.ts
+++ client/src/data/guestData.ts
@@ -30,15 +30,18 @@ export const invitation: Invitation = {
   receptionAddress: "3520 Seagate Way\nOceanside, CA 92056",
   storyText: "Our paths crossed on a beautiful spring morning at the local Walmart in Escondido. Hamza was searching for the perfect spices for his pasta, while Andrea was looking for fresh ingredients for a special dinner. A chance encounter over the most beautiful peonies led to our first conversation.",
   proposalText: "Under the same stars where we first shared our dreams, Hamza got down on one knee in our favorite spot overlooking the ocean. With tears of joy and hearts full of love, Andrea said yes to forever together.",
-  photos: [
-     "https://i.postimg.cc/kgDxkhyZ/5c58ba37-3836-4212-95bd-2caac37d7514.jpg",
-    "https://i.postimg.cc/vmBS2z1H/2e017fa1-b21c-4ac3-b297-7527991d3717.jpg",
-    "https://i.postimg.cc/m2gMH6KD/27cd7684-4839-47e8-a75e-67f1a246046e.jpg",
-    "https://i.postimg.cc/FK3JvtkP/5c0a14dc-24ae-48d6-a81d-7aa3c2d7c7c7.jpg",
-    "https://i.postimg.cc/NMt2fN1Q/04b3ffcc-a37c-4442-85c8-c1d1d59533db.jpg",
-    "https://i.postimg.cc/1X5gDfCT/4da7a5e3-9567-4e85-a4c0-a0cc5e22e056.jpg",
-    "https://i.postimg.cc/XqzvFxyG/468a85cc-7581-4bd2-be29-785f443fcc88.jpg",
-  ]
+   photos: [
+    "/photos/ph-0.jpg",
+    "/photos/ph-1.jpg",
+    "/photos/ph-2.jpg",
+    "/photos/ph-3.jpg",
+    "/photos/ph-4.jpg",
+    "/photos/ph-5.jpg",
+    "/photos/ph-6.jpg",
+    "/photos/ph-7.jpeg",
+    "/photos/ph-8.jpeg",
+    "/photos/ph-9.jpeg",
+  ],
 };
 
 export const guests: Guest[] = [
